3eaa566f7380855b902282580d91ac2618673f25
Edgar Asatryan <edgarartur2000@mail.ru>
2024-05-19 15:53:38 +0400

    feat:

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Query } from '@nestjs/common';
+import { Body, Controller, Query } from '@nestjs/common';
 import { successResponse } from 'src/common/utils/apiResponse';
 import { Auth } from 'src/iam/authentication/decorators/auth.decorator';
 import { AuthType } from 'src/iam/authentication/enums/auth-type.enum';
 import { User } from 'src/iam/decorators/user.decorator';
 import { CartService } from './cart.service';
+import { AddCartProductDto } from './dto/add-cart-product.dto';
 import { CartFiltersDto } from './dto/cart-filters.dto';
+import { DeleteCartProductDto } from './dto/delete-cart-product.dto';
+import { UpdateCartProductDto } from './dto/update-cart-product.dto';
 
 @Auth(AuthType.JWT)
 @Controller('cart')
@@ -29,5 +32,29 @@ export class CartController {
       })),
       totalPages,
     });
+  }
+
+  async add(
+    @User('id') userId: number,
+    @Body() addCartProductDto: AddCartProductDto,
+  ) {
+    await this.cartService.add(userId, addCartProductDto);
+    return successResponse('Товар добавлен в корзину');
+  }
+
+  async update(
+    @User('id') userId: number,
+    @Body() updateCartProductDto: UpdateCartProductDto,
+  ) {
+    await this.cartService.update(userId, updateCartProductDto);
+    return successResponse('Товар успешно обновлен');
+  }
+
+  async delete(
+    @User('id') userId: number,
+    @Body() deleteCartProductDto: DeleteCartProductDto,
+  ) {
+    await this.cartService.delete(userId, deleteCartProductDto);
+    return successResponse('Товар удален из корзины');
   }
 }
